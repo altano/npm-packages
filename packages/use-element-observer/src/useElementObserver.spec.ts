f2b67573f14f5780ae3f52fa19e41c1fa1bdda7d
Alan Norbauer <altano@gmail.com>
2022-07-14 22:40:49 -0700

    Remove extra files

--- packages/use-element-observer/src/useElementObserver.spec.ts
+++
@@ -1,7 +0,0 @@
-import { useElementObserver } from "./useElementObserver";
-
-describe("useElementObserver", () => {
-  it("should work", () => {
-    expect(useElementObserver()).toEqual("use-element-observer");
-  });
-});
