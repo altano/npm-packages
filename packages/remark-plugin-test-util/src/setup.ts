1c013a64d214ecd94fc164c34ecfd80005867bd1
Alan Norbauer <altano@gmail.com>
2022-07-31 20:29:42 -0700

    test: fix absolute paths in error throwing tests

--- packages/remark-plugin-test-util/src/setup.ts
+++ packages/remark-plugin-test-util/src/setup.ts
@@ -7,6 +7,38 @@ import type { Options } from "prettier";
 
 type Serializer = Parameters<typeof expect.addSnapshotSerializer>[0];
 
+function getSourceDir(): string {
+  const sourceDir = process.env["npm_config_local_prefix"];
+  if (sourceDir == null) {
+    throw new Error(`npm_config_local_prefix env variable must be set`);
+  }
+  return sourceDir;
+}
+
+/**
+ * Strip absolute paths from the output to make tests portable
+ */
+function stripAbsolutePaths(str: string): string {
+  return str.replaceAll(getSourceDir(), "<repo-root>");
+}
+
+const StringSerializer: Serializer = {
+  serialize(val: string, config, indentation, depth, refs, printer) {
+    return printer(stripAbsolutePaths(val), config, indentation, depth, refs);
+  },
+  test(val) {
+    return (
+      typeof val === "string" &&
+      // If we don't test for the string we're stripping we'll hit infinite
+      // recursion. I'm not sure why: the `printer` function must include this
+      // serializer, which is obviously wrong?
+      val.includes(getSourceDir())
+    );
+  },
+};
+
+expect.addSnapshotSerializer(StringSerializer);
+
 const VFileSerializer: Serializer = {
   serialize(val: VFile, _config, _indentation, _depth, _refs, _printer) {
     const contents = val.toString();
@@ -14,13 +46,7 @@ const VFileSerializer: Serializer = {
       parser: "babel",
       ...(prettierConfig as Options),
     });
-
-    // Strip absolute paths from the output to make tests portable
-    const sourceDir = process.env["npm_config_local_prefix"];
-    if (sourceDir == null) {
-      throw new Error(`npm_config_local_prefix env variable must be set`);
-    }
-    return formatted.replaceAll(sourceDir, "<repo-root>");
+    return stripAbsolutePaths(formatted);
   },
   test(val) {
     return val instanceof VFile;
