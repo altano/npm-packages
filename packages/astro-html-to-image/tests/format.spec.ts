a8a6fdafa396433bb4baa65d7744d7520ebd6a87
Alan Norbauer <altano@gmail.com>
2023-08-27 23:25:45 -0700

    node: 18.16.0 => 18.17.1

--- packages/astro-html-to-image/tests/format.spec.ts
+++ packages/astro-html-to-image/tests/format.spec.ts
@@ -1,15 +1,8 @@
+import { basicHtml } from "./../../astro-opengraph-image/tests/utils/basicHtml";
 import { describe, expect } from "vitest";
 import { middleware } from "./utils/makeTest";
 
 describe("format", async () => {
-  const basicHtml = `
-    <html>
-      <body style="background-color: white; width: 100vw; height: 100vh;">
-        <h1 style="font-size: 3.5rem; margin: 0;">Why hello there.</h1>
-      </body>
-    </html>
-  `.trim();
-
   middleware.should("convert png", {
     requestUrl: `http://example.com/test.png`,
     format: "png",
