d56ea7bd746ae1a1a3f5a7436d7d02c271aa5674
Alan Norbauer <altano@gmail.com>
2024-08-31 18:37:36 -0700

    chore: add arethetypeswrong checking to use-* react libs
    Note that these libs had incorrect types according to arethetypeswrong. This must have something to do with the react-library tsconfig not bundling the types together. Rather than investigate, I'm just switching to tsup's `dts: true` type generation which seems to work.

--- packages/build-config/tsup.config.browser.ts
+++ packages/build-config/tsup.config.browser.ts
@@ -4,8 +4,13 @@ export default defineConfig({
   // since we're bundling, index is the only entry
   entry: ["./src/index.ts"],
   format: "esm",
-  onSuccess: "pnpm build:types",
-  dts: false,
+
+  // Because we're bundling, tsc can't generate types for us, so we use tsup's
+  // type gen. The downside is that we don't get declaration maps. Unfortunately
+  // we can't use tsc to ONLY generate declaration maps (and they would be wrong
+  // because it doesn't work on the bundled file anyway).
+  dts: true,
+
   clean: true,
 
   platform: "browser",
