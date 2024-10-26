156e984add3af9d6c0905c9885fde706c7b92868
Alan Norbauer <altano@gmail.com>
2024-10-25 20:16:30 -0700

    chore: lint consistent dependencies w/ syncpack

---
+++ .syncpackrc.js
@@ -0,0 +1,42 @@
+// @ts-check
+
+/** @type {import("syncpack").RcFile} */
+const config = {
+  lintFormatting: false,
+  semverGroups: [
+    {
+      packages: ["package-time-traveler"],
+      isIgnored: true,
+    },
+    {
+      dependencyTypes: ["local"],
+      isIgnored: true,
+    },
+    {
+      label: "Examples w/ exact versions",
+      packages: ["!@altano/**"],
+      dependencies: ["**"],
+      dependencyTypes: ["!local"],
+      range: "",
+    },
+  ],
+  versionGroups: [
+    {
+      packages: ["package-time-traveler"],
+      isIgnored: true,
+    },
+    {
+      dependencyTypes: ["local"],
+      isIgnored: true,
+    },
+    {
+      label: "Local package dependencies",
+      packages: ["@altano/**"],
+      dependencies: ["$LOCAL"],
+      dependencyTypes: ["!local"],
+      pinVersion: "workspace:*",
+    },
+  ],
+};
+
+export default config;
