7812fc096931f4f6e20b3c89ec9ac564ed5f3bce
Alan Norbauer <altano@gmail.com>
2025-03-20 01:43:35 -0700

    add prettier-plugin-jsdoc (disabled)

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+/**
+ * @type {import("prettier").Config}
+ * @see https://prettier.io/docs/configuration
+ */
+export default {
+  printWidth: 80,
+  semi: true,
+  trailingComma: "all",
+  // jsdocPrintWidth: 80,
+  plugins: [
+    // TODO Consider enabling this
+    // "prettier-plugin-jsdoc",
+  ],
+};
