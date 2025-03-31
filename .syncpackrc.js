cffd41b9618989a06ca955a8216ad8b0f8adc65c
Alan Norbauer <altano@gmail.com>
2025-03-30 18:52:00 -0700

    use pnpm catalogs

--- .syncpackrc.js
+++ .syncpackrc.js
@@ -1,4 +1,44 @@
 // @ts-check
+import { readFileSync } from "node:fs";
+import semver from "semver";
+import { parse } from "yaml";
+
+/**
+ * @returns {NonNullable<import("syncpack").RcFile['versionGroups']>}
+ */
+function getPnpmCatalogPeerDependencies() {
+  const workspaceConfig = readFileSync("pnpm-workspace.yaml", "utf8");
+  const pnpmWorkspaceConfig = parse(workspaceConfig);
+  /**
+   * @type {import("syncpack").RcFile['versionGroups']}
+   */
+  return Object.entries(pnpmWorkspaceConfig.catalog).flatMap(
+    ([packageName, versionStr]) => {
+      const minVersion = semver.minVersion(versionStr);
+      if (minVersion == null) {
+        throw new Error(
+          `Invalid semver range "${versionStr} for package ${packageName}"`,
+        );
+      }
+
+      return [
+        {
+          label: `Enforce pnpm default catalog for ${packageName} (regular deps)`,
+          packages: ["!**/*-example"], // excludes example w/ exact versions
+          dependencies: [packageName],
+          dependencyTypes: ["!local", "!peer"],
+          pinVersion: "catalog:",
+        },
+        {
+          label: `Enforce pnpm default catalog for ${packageName} (peer deps)`,
+          dependencies: [packageName],
+          dependencyTypes: ["peer"],
+          pinVersion: `${minVersion.major}.x.x`,
+        },
+      ];
+    },
+  );
+}
 
 /** @type {import("syncpack").RcFile} */
 const config = {
@@ -19,7 +59,7 @@ const config = {
     },
     {
       label: "Examples w/ exact versions",
-      packages: ["!@altano/**"],
+      packages: ["*-example"],
       dependencies: ["**"],
       dependencyTypes: ["!local"],
       range: "",
@@ -34,6 +74,7 @@ const config = {
       dependencyTypes: ["local"],
       isIgnored: true,
     },
+    ...getPnpmCatalogPeerDependencies(),
     {
       label: "Local package dependencies",
       packages: ["@altano/**"],
