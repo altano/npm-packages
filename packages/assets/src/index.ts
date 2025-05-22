41ee452d8bd34f06f5c0610a2f5e9e92ec4bcaed
Alan Norbauer <altano@gmail.com>
2025-05-22 17:27:31 -0400

    WIP: depcheck -> unplugin-unused

--- packages/assets/src/index.ts
+++ packages/assets/src/index.ts
@@ -1,13 +1,10 @@
-import { createRequire } from "node:module";
 import { readFile } from "node:fs/promises";
-
-const resolve = createRequire(import.meta.url).resolve;
 
 /**
  * @returns the file path for the Inter woff font file at the given weight
  */
 export function getInterPath(fontWeight: number): string {
-  return resolve(
+  return import.meta.resolve(
     `@fontsource/inter/files/inter-latin-${fontWeight}-normal.woff`,
   );
 }
--- packages/build-config/tsdown.config.base.ts
+++ packages/build-config/tsdown.config.base.ts
@@ -1,8 +1,19 @@
 import type { UserConfig } from "tsdown";
+import UnpluginUnused from "unplugin-unused/rolldown";
 
 const config: UserConfig = {
   dts: true,
   clean: true,
+  inputOptions: {
+    plugins: [
+      UnpluginUnused({
+        level: "error",
+        ignore: [
+          "@fontsource/inter", // doesn't seem to understand import.meta.resolve
+        ],
+      }),
+    ],
+  },
 };
 
 export default config;
