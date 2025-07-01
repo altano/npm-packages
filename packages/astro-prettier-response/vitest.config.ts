7c264c8401774df4727a426e1b53cb3e095ea996
Alan Norbauer <altano@gmail.com>
2025-07-01 01:04:48 -0700

    astro-prettier-response: fix unit test timeouts

--- packages/astro-prettier-response/vitest.config.ts
+++ packages/astro-prettier-response/vitest.config.ts
@@ -6,6 +6,8 @@ import {
 const config: TestingConfig = deriveConfigWithoutPlugins({
   test: {
     setupFiles: ["./tests/unit/utils/setup.ts"],
+    hookTimeout: 120_000, // building the astro test fixtures might take a while in ci
+    testTimeout: 20_000,
   },
 });
 
